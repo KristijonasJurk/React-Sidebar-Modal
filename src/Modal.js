ba2858624a1743cff506c2fcadc29107c1c909d1
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-20 15:04:15 +0200

    basic struture, no functionality yet

--- src/Modal.js
+++ src/Modal.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 const Modal = () => {
-  return <div className={``}>
-
+  return <div className={`modal-overlay`}>
+    <div className="modal-container">
+      <h3>modal content</h3>
+      <button className="close-modal-btn">
+        <FaTimes />
+      </button>
+    </div>
   </div>
 }
 
